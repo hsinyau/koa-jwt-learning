9ee60c0ce9b1aa39b01b78e0daa825c5853300df
H52e5 <2737164694@qq.com>
2022-08-01 23:22:56 +0800

    chore: Environment variable

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 const Koa = require('koa')
 const app = new Koa()
 
+const { APP_PORT } = require('./config/config.default')
+
 app.use((ctx, next) => {
   ctx.body = "hello"
 })
 
-app.listen(2333, () => {
+app.listen(APP_PORT, () => {
   console.log("server is runing on http://localhost:2333")
 })
