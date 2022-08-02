d5c7170d340b1c7cd723e4e901073be5bfbea74a
H52e5 <2737164694@qq.com>
2022-08-02 15:40:09 +0800

    chore: Verify the login

--- src/constant/err.types.js
+++ src/constant/err.types.js
@@ -13,5 +13,20 @@ module.exports = {
     code: 10003,
     message: '用户注册错误',
     data: ''
+  },
+  userDoesNotExist: {
+    code: 10004,
+    message: '用户不存在',
+    data: ''
+  },
+  userLoginError: {
+    code: 10005,
+    message: '用户登录失败',
+    data: ''
+  },
+  invalidPassword: {
+    code: 10006, 
+    message: "密码不匹配",
+    data: ''
   }
 }
--- src/middleware/user.middleware.js
+++ src/middleware/user.middleware.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcryptjs')
 const { getUserInfo } = require('../service/user.service')
-const { userFormateError, userAlreadyExited, userRegisterError } = require('../constant/err.types')
+const { userFormateError, userAlreadyExited, userRegisterError, userDoesNotExist, userLoginError, invalidPassword } = require('../constant/err.types')
 const { emit } = require('nodemon')
 
 const userValidator = async (ctx, next) => {
@@ -43,8 +43,37 @@ const crpytPassword = async (ctx,next) => {
   await next()
 }
 
+const verifyLogin = async (ctx, next) => {
+  // 判断用户是否存在(不存在就报错)
+  // 密码是否匹配(不匹配就报错)
+  const { user_name, password } = ctx.request.body
+
+  try {
+    const res = await getUserInfo({ user_name })
+
+    if(!res) {
+      console.error("用户名不存在", { user_name })
+      ctx.app.emit('error', userDoesNotExist, ctx)
+      return
+    }
+
+    if(!bcrypt.compareSync(password, res.password)){
+      ctx.app.emit('error', invalidPassword, ctx)
+      return
+    }
+    
+  } catch (err) {
+    console.error(err)
+    ctx.app.emit('error', userLoginError, ctx)
+    return
+  }
+  
+  await next()
+}
+
 module.exports = {
   userValidator,
   verifyUser,
-  crpytPassword
+  crpytPassword,
+  verifyLogin
 }
