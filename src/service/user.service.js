1fec5a37bdf97878597de042b7d01f0a855f3c93
H52e5 <2737164694@qq.com>
2022-08-02 13:35:43 +0800

    chore: Error handling

--- src/service/user.service.js
+++ src/service/user.service.js
@@ -1,10 +1,28 @@
 const User = require('../model/user.model')
 
 class UserService {
+  // 创建用户
   async createUser(user_name, password) {
     const res = await User.create({ user_name, password })
     // console.log(res)
     return res.dataValues
+  }
+
+  // 查询用户信息
+  async getUserInfo({id, user_name, password, is_admin}){
+    const whereOpt = {}
+
+    id && Object.assign(whereOpt, { id })
+    user_name && Object.assign(whereOpt, { user_name })
+    password && Object.assign(whereOpt, { password })
+    is_admin && Object.assign(whereOpt, { is_admin })
+
+    const res = await User.findOne({
+      attributes: ['id', 'user_name', 'password', 'is_admin'],
+      where: whereOpt
+    })
+
+    return res ? res.dataValues : null
   }
 }
 
